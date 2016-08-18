b460265b6658c2acd8d779904c399b09261dbe0d
Yan Uehara <yanuehara@hotmail.com>
2016-08-17 22:58:29 -0400

    Corrected code to conform to PEP 8 and minor corrections

--- trabalho.py
+++ trabalho.py
@@ -10,6 +10,7 @@ if pd.__version__ != '0.18.1':
     print "Esse script necessita do pandas == 0.18.1"
     exit()
 
+
 class Dataset:
     def __init__(self, filename):
         self.con = sqlite3.connect(filename)
@@ -85,28 +86,29 @@ WHERE
         self.pre_process_position()
 
     def generate_target_classes(self):
-        #Calculates the target variable
+        # Calculates the target variable
         goals_balance = []
         for goal in self.matches["home_team_goal"] - self.matches["away_team_goal"]:
-            if goal<=0:
+            if goal <= 0:
                 goals_balance.append(0)
             else:
                 goals_balance.append(1)
 
-        self.matches["goals_balance"]=pd.Series(goals_balance)
+        self.matches["goals_balance"] = pd.Series(goals_balance)
 
         del self.matches["home_team_goal"]
         del self.matches["away_team_goal"]
 
     def pre_process_player_stats(self):
-        #Substitutes the home_team_playerXX and away_team_playerXX for the historical max overall_rating
-        player_stats=pd.read_sql_query("SELECT player_api_id, max(overall_rating) as overall_rating FROM Player_Stats GROUP BY player_api_id;",
-                                            self.con, index_col="player_api_id")
+        # Substitutes the home_team_playerXX and away_team_playerXX for the historical max overall_rating
+        player_stats = pd.read_sql_query(
+            "SELECT player_api_id, max(overall_rating) as overall_rating FROM Player_Stats GROUP BY player_api_id;",
+            self.con, index_col="player_api_id")
         for i in range(len(self.matches)):
             print "Running pre-process for match %s" % i
-            for j in range(1,12):
-                self.matches.loc[i,"home_player_%s" % j] = \
-                    player_stats.loc[self.matches.loc[i,"home_player_%s" % j], "overall_rating"  ]
+            for j in range(1, 12):
+                self.matches.loc[i, "home_player_%s" % j] = \
+                    player_stats.loc[self.matches.loc[i, "home_player_%s" % j], "overall_rating"]
             for j in range(1, 12):
                 self.matches.loc[i, "away_player_%s" % j] = \
                     player_stats.loc[self.matches.loc[i, "away_player_%s" % j], "overall_rating"]
@@ -116,69 +118,71 @@ WHERE
         self.formations[0] = list()
         self.formations[1] = list()
 
-        for i in range(  len(self.matches) ):
+        for i in range(len(self.matches)):
             home_players_y = list()
             away_players_y = list()
 
             for j in range(1, 12):
-                home_players_y.append(self.matches.loc[i,'home_player_Y%d' % j])
-                away_players_y.append(self.matches.loc[i,'away_player_Y%d' % j])
+                home_players_y.append(self.matches.loc[i, 'home_player_Y%d' % j])
+                away_players_y.append(self.matches.loc[i, 'away_player_Y%d' % j])
 
             players_y = [home_players_y, away_players_y]
 
-            for i in range(2):
-                formation_dict = Counter(players_y[i])
+            for j in range(2):
+                formation_dict = Counter(players_y[j])
                 sorted_keys = sorted(formation_dict)
                 formation = ''
                 for key in sorted_keys[1:-1]:
                     y = formation_dict[key]
                     formation += '%d-' % y
                 formation += '%d' % formation_dict[sorted_keys[-1]]
-                self.formations[i].append(formation)
+                self.formations[j].append(formation)
 
             print('Home team formation: ' + self.formations[0][-1])
             print('Away team formation: ' + self.formations[1][-1])
 
         for i in range(1, 12):
-          del self.matches["home_player_Y%s" % i]
+            del self.matches["home_player_Y%s" % i]
 
         for i in range(1, 12):
-          del self.matches["away_player_Y%s" % i]
+            del self.matches["away_player_Y%s" % i]
 
     def pre_process_possession(self):
-      possession_ = []
-      nullposs = 0
-      for i in range(len(self.matches) ):
-        xml = xmltodict.parse(self.matches.loc[i,"possession"])
-        #print "Running possession for game: %d" % i
+        possession_ = []
+        nullposs = 0
+        for i in range(len(self.matches)):
+            xml = xmltodict.parse(self.matches.loc[i, "possession"])
+            # print "Running possession for game: %d" % i
 
-        if xml["possession"] == None:
-          possession = 50
-          nullposs+=1
-          #continue
-        else:
-          if type(xml["possession"]["value"]) == list:
-            if xml["possession"]["value"][-1].has_key("homepos") == True:
-              possession = xml["possession"]["value"][-1]["homepos"]
+            if xml["possession"] is None:
+                possession = 50
+                nullposs += 1
+                # continue
             else:
-              possession = 50
-              nullposs+=1
-          else:
-            possession = xml["possession"]["value"]["homepos"]
-        possession_.append(possession)
+                if type(xml["possession"]["value"]) == list:
+                    if "homepos" in xml["possession"]["value"][-1]:
+                        possession = xml["possession"]["value"][-1]["homepos"]
+                    else:
+                        possession = 50
+                        nullposs += 1
+                else:
+                    possession = xml["possession"]["value"]["homepos"]
+            possession_.append(possession)
 
-      self.matches["possession"] = pd.Series(possession_)
-      self.matches["possession"] = self.matches["possession"].astype(np.int64, copy=True)
+        self.matches["possession"] = pd.Series(possession_)
+        self.matches["possession"] = self.matches["possession"].astype(np.int64, copy=True)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     dataset = Dataset("database.sqlite")
     dataset.pre_process()
 
-    del dataset.matches["season"] #Removing unused column
-    X_train, X_test, y_train, y_test = train_test_split(dataset.matches.drop("goals_balance", axis=1), dataset.matches.loc[:,"goals_balance"]  , test_size=0.3)
-    predictor=KNeighborsClassifier(n_neighbors=11)
+    del dataset.matches["season"]  # Removing unused column
+    X_train, X_test, y_train, y_test = train_test_split(dataset.matches.drop("goals_balance", axis=1),
+                                                        dataset.matches.loc[:, "goals_balance"], test_size=0.3)
+    predictor = KNeighborsClassifier(n_neighbors=11)
     predictor.fit(X_train, y_train)
 
-    y_pred=predictor.predict(X_test)
+    y_pred = predictor.predict(X_test)
 
-    print (y_pred==y_test).value_counts(normalize=True)*100
+    print (y_pred == y_test).value_counts(normalize=True) * 100
